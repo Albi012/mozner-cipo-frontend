9e760e10bf7aa5c363cd27ca408df05e009ad02e
Albi012 <mayeralbert12@gmail.com>
2020-03-24 13:57:39 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return (
+        <div >
+            <h1 className="ui header">Mózner cipő!</h1>
+        </div>
+    )
+}
+
+export default Header;
